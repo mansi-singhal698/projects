bb409781623c9b386301a4fb0e540b669d7e95ba
mansi-singhal698 <singhalmansi817@gmail.com>
2025-03-26 01:10:19 +0530

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,14 @@
+# typecasting: the conversion of one data type into the other data type is known as typecasting
+# python supports a wide variety of functions like : int(),float(),str(),ord(),hex(),oct(),tuple(),set(),list(),dict() etc.
+# Explicit typecasting
+a = "1"
+b = "2"
+print(int(a)+int(b))
+
+# Explicit typecasting : data type conversion done via developer 
+# Implicit typecasting : data type conversion done by self or python
+
+#implicit typecasting
+c = 1.9
+d = 8
+print(c+d)
