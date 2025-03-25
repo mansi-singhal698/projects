6719f572de6689547c9e78956217a8f857d7ebe6
mansi-singhal698 <singhalmansi817@gmail.com>
2025-03-26 00:18:08 +0530

    data_type and variable

---
+++ variable_datatype.py
@@ -0,0 +1,30 @@
+# what is variable
+# variable is like a conatiner that holds data.
+b = True
+c = "mansi"
+d = None
+
+# what is a Data Type?
+# data type specifies the type of value a varible holds. In python, we can print the type of any operator using type functions:
+a = 1
+print(type(a))
+
+# complex number
+n = complex(2,8)
+print(n)
+
+#sequenced data : list and tuple
+#list : A list is an ordered collection of data with elements separated by a comma and enclosed within square brackets list is mutable
+
+list1 = [9,8,[8,7]]
+print(list1)
+
+#tuple : tuple is exact like list but it is immutable
+
+tuple1 = (("parrot","sparrow"),("tiger","lion"))
+print(tuple1)
+
+# Mapped data
+# dict : A dicitionary is an unordered collection of data containing a key value pair . The key vaalues pairs are enclosed within curly bracketstt
+dict1 = {"name":"mansi","age":20,"canvote":True}
+print(dict1)
