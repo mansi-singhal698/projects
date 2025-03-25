ba21f1021f881a2f5ea3cb39e10e103d0fdaad77
mansi-singhal698 <singhalmansi817@gmail.com>
2025-03-26 00:35:55 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#Arithmetic operator
+print(15+6)
+print(15-6)
+print(15/6)
+print(15//6)
+print(5**3)
+print(5%3)
+
+# exercise-1 - Create a calculator capable of performing addition,subtraction,multiplication and division operations on two numbers. your program should format the output in a readabe manner!
+
+a = 20
+b = 10
+
+print("Addition :",a+b)
+print("subtraction :",a-b)
+print("multiplication :",a*b)
+print("Division :",a/b)
