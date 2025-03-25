a4ea5d9edde9b3e68fa2eeb780bbfd5cc56775b1
mansi-singhal698 <singhalmansi817@gmail.com>
2025-03-25 23:19:45 +0530

    sequence character

---
+++ second.py
@@ -0,0 +1,21 @@
+# multi-line comment
+'''
+
+hlo i am mansi
+hlo i am mansi
+hlo i am mansi
+hlo i am mansi
+
+'''
+print("hey I am a good girl\nand currently pursuing btech ")  #\n is sequence charcter,it is use for next line
+
+# To insert characters that cannot be directly used in a string, we use an escape sequence character
+# An escape sequence character is a backslash \ followed by the character you want to insert.
+print("hey i am a \"good girl \"and this viewer is also a good girl")
+
+# More or print statement
+print("hey",6,7)
+
+# we can use separator also in it
+print("hey",6,7,sep='~',end="00")
+
